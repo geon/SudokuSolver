1d25701df311d7cd7a93e394cd5dc2dd83ff23b0
Victor Widell <victor@topmost.se>
2014-10-19 11:24:47 +0200

    Initial Commit

---
+++ SudokuSolver/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SudokuSolver
+//
+//  Created by Victor Widell on 2014-10-19.
+//  Copyright (c) 2014 Victor Widell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
