3187d78fa59f407f98c819f2ef7aa609971674d5
Victor Widell <victor@topmost.se>
2014-10-19 13:36:23 +0200

    Added sample boards.

--- SudokuSolver/main.c
+++ SudokuSolver/main.c
@@ -196,40 +196,89 @@ int setConstrainedCells (Board board) {
 
 int main (int argc, const char * argv[]) {
 	
-	int board[9][9] = {
+	// Puzzles from www.websudoku.com
+	
+	Board easy = {
 		{0,0,1,  3,2,4,  0,0,0},
 		{0,0,0,  0,0,6,  0,0,4},
 		{9,2,0,  0,8,0,  3,0,6},
-
+		
 		{1,0,0,  2,0,8,  5,6,0},
 		{8,0,0,  0,5,0,  0,0,1},
 		{0,6,2,  9,0,1,  0,0,8},
-
+		
 		{3,0,9,  0,1,0,  0,7,2},
 		{4,0,0,  8,0,0,  0,0,0},
 		{0,0,0,  7,3,9,  4,0,0}
 	};
 	
-//	int x = 6;
-//	int y = 5;
-//	
-//	printPossibilities(rowPossibilities(board, y));
-//	printPossibilities(colPossibilities(board, x));
-//	printPossibilities(tilePossibilities(board, x/3, y/3));
+	Board medium = {
+		{0,4,0,  1,2,0,  0,9,0},
+		{0,0,3,  0,5,0,  0,0,0},
+		{9,5,0,  8,6,0,  0,0,0},
+		
+		{8,0,9,  0,0,0,  0,0,3},
+		{0,0,0,  3,0,6,  0,0,0},
+		{6,0,0,  0,0,0,  5,0,2},
+		
+		{0,0,0,  0,3,8,  0,2,1},
+		{0,0,0,  0,7,0,  6,0,0},
+		{0,9,0,  0,1,5,  0,3,0}
+	};
 	
-//	Possibilities p = cellPossibilities(board, x, y);
-//	
-//	printPossibilities(p);
-//	
-//	printf("%d\n", numPossibilities(p));
-//	printf("%d\n", constrainedValue(p));
+	Board hard = {
+		{8,0,1,  0,3,6,  2,0,0},
+		{2,0,0,  0,0,4,  0,9,0},
+		{0,0,9,  0,0,2,  0,0,0},
+		
+		{0,1,0,  0,0,0,  0,0,2},
+		{0,0,0,  6,0,8,  0,0,0},
+		{6,0,0,  0,0,0,  0,7,0},
+		
+		{0,0,0,  8,0,0,  1,0,0},
+		{0,2,0,  7,0,0,  0,0,4},
+		{0,0,5,  2,4,0,  6,0,3}
+	};
+	
+
+	Board evil = {
+		{2,0,0,  0,0,0,  4,0,0},
+		{0,0,1,  0,0,7,  0,0,0},
+		{0,0,0,  4,8,0,  5,0,2},
+		
+		{1,3,0,  0,4,0,  0,0,0},
+		{0,0,8,  2,0,1,  3,0,0},
+		{0,0,0,  0,3,0,  0,4,5},
+		
+		{6,0,7,  0,1,3,  0,0,0},
+		{0,0,0,  6,0,0,  8,0,0},
+		{0,0,4,  0,0,0,  0,0,1}
+	};
+	
+	Board *board = &hard;
 	
 	do {
 
-		printBoard(board);
+		printBoard(*board);
 
-	} while (setConstrainedCells(board));
+	} while (setConstrainedCells(*board));
+
 	
-    return 0;
+	//	int x = 6;
+	//	int y = 5;
+	//
+	//	printPossibilities(rowPossibilities(board, y));
+	//	printPossibilities(colPossibilities(board, x));
+	//	printPossibilities(tilePossibilities(board, x/3, y/3));
+	
+	//	Possibilities p = cellPossibilities(board, x, y);
+	//
+	//	printPossibilities(p);
+	//
+	//	printf("%d\n", numPossibilities(p));
+	//	printf("%d\n", constrainedValue(p));
+	
+    
+	return 0;
 }
 
