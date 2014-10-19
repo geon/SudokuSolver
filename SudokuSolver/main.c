08db9e90aaa66e301fc53a1eb2a51f7e5ffab594
Victor Widell <victor@topmost.se>
2014-10-19 12:35:32 +0200

    Added possible value calculation and printing.

--- SudokuSolver/main.c
+++ SudokuSolver/main.c
@@ -9,13 +9,105 @@
 #include <stdio.h>
 
 typedef int Board[9][9];
+typedef int Possibilities;
+
+
+Possibilities rowPossibilities (Board board, int y) {
+	
+	Possibilities p = ~0;
+	
+	for (int x = 0; x < 9; ++x) {
+		
+		int value = board[y][x];
+		
+		if (value) {
+			
+			p &= ~(1<<value);
+		}
+	}
+	
+	return p;
+}
+
+
+Possibilities colPossibilities (Board board, int x) {
+	
+	Possibilities p = ~0;
+	
+	for (int y = 0; y < 9; ++y) {
+		
+		int value = board[y][x];
+		
+		if (value) {
+			
+			p &= ~(1<<value);
+		}
+	}
+	
+	return p;
+}
+
+
+Possibilities tilePossibilities (Board board, int tx, int ty) {
+	
+	Possibilities p = ~0;
+	
+	for (int y = 0; y < 3; ++y) {
+		
+		for (int x = 0; x < 3; ++x) {
+
+			
+			int value = board[ty*3 + y][tx*3 + x];
+			
+			if (value) {
+				
+				p &= ~(1<<value);
+			}
+		}
+	}
+	
+	return p;
+}
+
+
+Possibilities cellPossibilities (Board board, int x, int y) {
+	
+	return
+	rowPossibilities(board, y) &
+	colPossibilities(board, x) &
+	tilePossibilities(board, x/3, y/3);
+}
+
+
+void printPossibilities (Possibilities p) {
+
+	int first = 1;
+	
+	for (int i = 1; i <= 9; ++i) {
+		
+		if (1<<i & p) {
+		
+			if (!first) {
+
+				printf(", ");
+			}
+		
+			first = 0;
+			
+			printf("%d", i);
+		}
+	}
+
+	printf("\n");
+}
+
 
 void printBoard (Board board) {
 	
 	for (int y = 0; y < 9; ++y) {
 		
 		for (int x = 0; x < 9; ++x) {
-		
+			
 			int cell = board[y][x];
 			
 			if (cell) {
@@ -27,17 +119,13 @@ void printBoard (Board board) {
 				printf("  ");
 			}
 		}
-
+		
 		printf("\n");
 	}
 }
 
 
-
-int main(int argc, const char * argv[])
-{
-
-
+int main (int argc, const char * argv[]) {
 	
 	int board[9][9] = {
 		{0,0,1,  3,2,4,  0,0,0},
@@ -52,10 +140,13 @@ int main(int argc, const char * argv[])
 		{4,0,0,  8,0,0,  0,0,0},
 		{0,0,0,  7,3,9,  4,0,0}
 	};
-	
 
 	printBoard(board);
 	
+	printPossibilities(rowPossibilities(board, 0));
+	printPossibilities(colPossibilities(board, 0));
+	printPossibilities(tilePossibilities(board, 0, 0));
+	printPossibilities(cellPossibilities(board, 0, 0));
 	
     return 0;
 }
